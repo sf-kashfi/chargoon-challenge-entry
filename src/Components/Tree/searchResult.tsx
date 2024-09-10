62224995947114e4a6fee6bc6fdbee9987bc0a4d
Fatemeh <kashfi.sf@gmail.com>
2024-09-10 23:39:25 +0330

    task number 4

--- src/Components/Tree/searchResult.tsx
+++ src/Components/Tree/searchResult.tsx
@@ -4,24 +4,25 @@ import ArrowDownIcon from "../SvgIcons/arrow-down";
 import ArrowUpIcon from "../SvgIcons/arrow-up";
 interface Props {
   items: (NodeType & { hierarchy: string[] })[];
-
 }
 
-function SearchResult({ items,  }: Props) {
+function SearchResult({ items }: Props) {
   const [isOpen, setIsOpen] = useState(true);
   const toggleWindow = () => {
     setIsOpen(!isOpen);
   };
   return (
-    <div className="search-result" style={{ height: isOpen? 200 : 0 , overflow: "auto" }}>
-      <div style={{width: '100%', textAlign: 'center'}} onClick={toggleWindow}>
-		<div style={{width: '20px', height: '20px',}}>
-          {isOpen ? <ArrowDownIcon /> : <ArrowUpIcon />}
-		</div>
+    <div className="search-result">
+      <div className="arrow-icon-container" onClick={toggleWindow}>
+        {isOpen ? <ArrowDownIcon /> : <ArrowUpIcon />}
       </div>
-      {isOpen && items.map((item) => (
-        <div>{item.title}</div>
-      ))}
+
+      <div
+        className="search-result-content"
+        style={{ height: isOpen ? 200 : 0 }}
+      >
+        {isOpen && items.map((item) => <div>{item.title}</div>)}
+      </div>
     </div>
   );
 }
